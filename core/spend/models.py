b1dd760ca03310bc9eaed04f9691850c2eb0e2f3
Oleksa_Peklo <kaplunalex1985@gmail.com>
2023-09-28 12:06:37 +0300

    Create main structure

---
+++ core/spend/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class SpendStatictic(models.Model):
+    name = models.CharField(max_length=255)   
+    date = models.DateField
+    spend = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    impressions = models.IntegerField(default=0)
+    clicks =  models.IntegerField(default=0)
+    conversions = models.IntegerField(default=0)
